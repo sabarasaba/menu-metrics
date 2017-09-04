b5184431d995425aa7782aa58754fe9063ef0116
Ignacio Rivas <rivasign@gmail.com>
2017-09-04 08:35:24 +0200

    Fix path

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const path = require('path')
 const menubar = require('menubar')
 
-console.log(path.join(__dirname, 'app/build/'));
-
 const mb = menubar({
-  dir: path.join(__dirname, 'app/build/')
+  dir: path.join(__dirname, 'app/build/'),
+  height: 600
 })
 
 mb.on('ready', function ready () {
