023cf2fa425b78aa081fcc465ce3cd30dc119074
Ignacio Rivas <rivasign@gmail.com>
2017-09-04 15:13:40 +0200

    Fix scrolling

--- app/src/components/navbar/styles.js
+++ app/src/components/navbar/styles.js
@@ -2,10 +2,6 @@ import { css } from 'emotion'
 
 export const root = css`
   display: flex;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
   justify-content: space-between;
   padding: 1rem;
   background-color: #FFF;
