6cfe7cf2f6bd41dfe101964a79075df1396c4276
Ignacio Rivas <rivasign@gmail.com>
2017-09-13 15:58:08 +0200

    Show valid token

--- src/components/api-token/index.js
+++ src/components/api-token/index.js
@@ -25,6 +25,7 @@ export default ({
           value: inputValue,
           disabled: !!isLoading
         }}
+        valid={!!apiKey}
         label="API Token"
         placeholder="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         meta={{
--- src/components/fields/input.js
+++ src/components/fields/input.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CheckCircle } from 'react-feather'
 import classNames from 'classnames/bind'
 
 import styles from './input.pss'
@@ -7,13 +8,14 @@ const cx = classNames.bind(styles)
 
 export default ({
   input,
+  valid,
   label,
   type,
   meta: { touched, error }
 }) => {
   return (
     <div className={cx('inputLine')}>
-      <label>{label}</label>
+      <label>{label} {valid && <span className={cx('valid')}><CheckCircle /></span>}</label>
       <div>
         <input {...input} type={type} placeholder={label} />
         {touched && error &&
